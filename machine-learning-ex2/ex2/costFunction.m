db852de43b13b6b1d584e28a97aa7a5e2b475171
Jiahua <jiahua.xu@outlook.com>
2018-02-27 15:12:46 +0800

    ex2: Logistic Regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,12 +20,21 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+y_hat = sigmoid(X * theta);
+% (m, 1)
+err = y_hat - y;
+% (n, 1) = (n, m) * (m, 1)
+grad = transpose(X) * err / m;
 
+% for loop version
+% for i = 1:m
+%     yi_hat = y_hat(i);
+%     J = J + (-1 * y(i) * log(yi_hat) - (1 - y(i)) * log(1-yi_hat));
+% end
+% J = J / m;
 
-
-
-
-
+% vectorilization version
+J = sum(-1 * y .* log(y_hat) - (1 - y) .* log(1-y_hat)) / m;
 
 % =============================================================
 
